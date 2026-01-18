5fc20bb2c2c9003f75751ce44daad26fb9b956be
ArnauM13 <ArnauM13@users.noreply.github.com>
2026-01-18 12:28:50 +0100

    chore(trans): added i18n files

--- FeverPets/src/app/features/pets/components/pet-card/pet-card.component.ts
+++ FeverPets/src/app/features/pets/components/pet-card/pet-card.component.ts
@@ -2,11 +2,12 @@ import { Component, input } from '@angular/core';
 import { CardComponent } from '../../../../shared/ui/card/card.component';
 import { Pet } from '../../models/pet.model';
 import { CommonModule } from '@angular/common';
+import { TranslateModule } from '@ngx-translate/core';
 
 @Component({
   selector: 'fp-pet-card',
   standalone: true,
-  imports: [CardComponent, CommonModule],
+  imports: [CardComponent, CommonModule, TranslateModule],
   template: `
     <fp-card
       class="w-full"
@@ -22,7 +23,7 @@ import { CommonModule } from '@angular/common';
         <div class="flex gap-2 mt-4">
           <button
             class="flex-1 px-4 py-2 bg-gray-200 hover:bg-gray-300 rounded transition-colors">
-            See more
+            {{ 'PETS.SEE_MORE' | translate }}
           </button>
         </div>
       </ng-template>
